608e1c36345bb9be9467d43c423a13abc6f2d41b
Akash namdeo <35231517+akashnamdeo@users.noreply.github.com>
2021-03-21 11:24:30 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+create conda env
+'''
+bash 
+conda create wineq -n python=3.7 -y
+'''
+activate env
+'''
+bash conda activate wineq
+'''
+create req file
+
+install req
+'''
+bash pip install -r requirements.txt
+'''
---
+++ template.py
@@ -0,0 +1,20 @@
+import os
+
+DIR=[os.path.join("data","raw"),
+     os.path.join("data","processsed"),
+     "data_given",
+     "notebooks",
+     "saved_models",
+     "src"]
+for dir in DIR:
+    os.makedirs(dir,exist_ok=True)
+    with open(os.path.join(dir,".gitkeep"),"w") as f:
+        pass
+
+files=["params.yaml",
+      "dvc.yaml",
+      os.path.join("src","__init__.py")]
+
+for file in files:
+    with open(file,"w") as f:
+        pass
